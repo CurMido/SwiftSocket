2968979ca0145d819602506473f2db62a4e35ac7
Jasoooon <351220761@qq.com>
2016-04-20 21:57:29 +0800

    20-04-16/21:51
    开始创建服务端了，这是第一步啊

---
+++ SocketServer/SocketServer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SocketServer
+//
+//  Created by 李伟 on 16/4/20.
+//  Copyright © 2016年 Jasoooon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SocketServer/SocketServer/main.swift
@@ -0,0 +1,72 @@
+//
+//  main.swift
+//  SocketServer
+//
+//  Created by 李伟 on 16/4/20.
+//  Copyright © 2016年 Jasoooon. All rights reserved.
+//
+
+
+import Foundation
+import CoreFoundation
+
+class main: NSObject ,NSStreamDelegate{
+    var inputStream:NSInputStream?
+    var outputStream:NSOutputStream?
+    
+    lazy var _chatMsgsArray:NSMutableArray = []
+    
+    func stream(aStream: NSStream, handleEvent eventCode: NSStreamEvent) {
+        switch eventCode {
+        case NSStreamEvent.OpenCompleted:
+            print("输入输出打开完成")
+            break;
+        case NSStreamEvent.HasBytesAvailable:
+            
+            break;
+        case NSStreamEvent.HasSpaceAvailable:
+            
+            break;
+        case NSStreamEvent.ErrorOccurred:
+            
+            break;
+        case NSStreamEvent.EndEncountered:
+            inputStream?.close()
+            outputStream?.close()
+            inputStream?.removeFromRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+            outputStream?.removeFromRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+            break;
+        default:
+            break;
+        }
+    }
+    
+    func ConnectToHost(){
+        let host:String = "127.0.0.1"
+        let port:UInt32 = 12345
+        
+        var readStream:Unmanaged<CFReadStreamRef>?
+        var writeStream:Unmanaged<CFWriteStreamRef>?
+        CFStreamCreatePairWithSocketToHost(nil, host, port, &readStream, &writeStream)
+        
+        inputStream = readStream?.takeUnretainedValue()
+        outputStream = writeStream?.takeUnretainedValue()
+        
+        inputStream?.delegate = self
+        outputStream?.delegate = self
+        
+        inputStream?.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+        outputStream?.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+        
+        inputStream?.open()
+        outputStream?.open()
+        
+    }
+    
+    func loginServer(){
+        let loginStr = "Iam:Jasoooon"
+        let data:NSData = loginStr.dataUsingEncoding(NSUTF8StringEncoding)!
+        outputStream?.write(UnsafePointer<UInt8>(data.bytes), maxLength: data.length)
+    }
+    
+}
